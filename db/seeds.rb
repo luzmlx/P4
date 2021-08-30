6d9966951e483b9f80a4b80312004abccc3b4f02
luzmlx <luzlopez0820@gmail.com>
2021-08-30 02:55:21 -0400

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CakeFlavor.destroy_all
+Cake.destroy_all
+User.destroy_all
+
+@admin = User.create!(first_name: 'Luz', last_name: 'Lopez', email: 'luzfinals24@gmail.com', password: '123456')
+
+puts "#{User.count} User created"
+
+Cake.create!(name: 'Chocolate Cake', description: 'Triple Layered Dark Chocolate Cake with Matcha Mousse and Frosting.
+Topped with Strawberries', price: 20, user: @admin)
+
+puts "#{Cake.count} Cake created"
+
+CakeFlavor.create!(name: 'sweet')
+CakeFlavor.create!(name: 'fruity')
+CakeFlavor.create!(name: 'savory')
+
+puts "#{CakeFlavor.count} flavors created"
+
+
+
